cb6134da2f16c6076a4a0e8a2deb41f8182e8e46
taimoor <taimoor.ali@conradlabs.com>
2020-10-03 01:10:15 +0500

    :loader: add specific loaders for sass

--- webpack.dev.js
+++ webpack.dev.js
@@ -22,6 +22,27 @@ module.exports = {
                 use : {
                     loader : "babel-loader"
                 }
+            },{
+                test: /\.s[ac]ss$/i,
+                use: [{
+                    loader: 'style-loader', // inject CSS to page
+                  }, {
+                    loader: 'css-loader', // translates CSS into CommonJS modules
+                  }, {
+                    loader: 'postcss-loader', // Run post css actions
+                    options: {
+                        postcssOptions:{
+                            plugins: function () { // post css plugins, can be exported to postcss.config.js
+                                return [
+                                    require('precss'),
+                                    require('autoprefixer')
+                                ];
+                            }
+                        }
+                    }
+                  }, {
+                    loader: 'sass-loader' // compiles Sass to CSS
+                  }]
             }
         ]
     },
